288f503756aed4d23e3e0bcb25b2b820ba166b54
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-26 20:57:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,71 +1,164 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
+class Song 
+  
+  attr_accessor :name, :artist_name 
+  @@all= []
+  
+  def self.all 
     @@all
-  end
-
+  end 
+  
   def save
-    self.class.all << self
-  end
-
-  def self.create 
-    song = self.new
-    self.all << song
-    song
-  end
-
-  def self.new_by_name(title)
+    self.class.all << self 
+  end 
+  
+  def self.create
+    song= self.new
+    self.all << song 
+    song 
+  end 
+  
+   def self.new_by_name(title)
     song = self.new
     song.name = title
+    
     song
   end
-
-  def self.create_by_name(title) 
-    song = self.create
+  
+    def self.create_by_name(title)
+    song= self.create 
     song.name = title
-    song
-  end
+    song 
+    end 
+  
+    def self.find_by_name(tittle)
+      search= self.all.find {|song| song.name == tittle}
+      search
+    end 
+    
+    def self.find_or_create_by_name(title)
+      search= self.find_by_name(title)
+        if search #equals self.find_by_name(title)
+        search
+      else 
+        self.create_by_name(title)
+      end 
+    end 
+  end 
+  
+  
+    def self.alphabetical(title)
+      @@all.
+      
+    end 
 
-  def self.find_by_name(title) 
-    result = self.all.detect {|song| song.name == title}
-    result
-  end
 
-  def self.find_or_create_by_name(title)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+#   attr_accessor :name, :artist_name
+#   @@all = []
+
+#   def self.all
+#     @@all
+#   end
+
+#   def save
+#     self.class.all << self
+#   end
+
+#   def self.create 
+#     song = self.new
+#     self.all << song
+#     song
+#   end
+
+#   def self.new_by_name(title)
+#     song = self.new
+#     song.name = title
+#     song
+#   end
+
+#   def self.create_by_name(title) 
+#     song = self.create
+#     song.name = title
+#     song
+#   end
+
+#   def self.find_by_name(title) 
+#     result = self.all.detect {|song| song.name == title}
+#     result
+#   end
+
+#   def self.find_or_create_by_name(title)
    
-    result = self.find_by_name(title)
-    if result
-      result
-    else
-      self.create_by_name(title)
-    end
-  end
+#     result = self.find_by_name(title)
+#     if result
+#       result
+#     else
+#       self.create_by_name(title)
+#     end
+#   end
 
-  def self.alphabetical
-    sorted = self.all.sort_by {|song| song.name}
-    sorted
-  end
+#   def self.alphabetical
+#     sorted = self.all.sort_by {|song| song.name}
+#     sorted
+#   end
 
-  def self.new_from_filename(filename)
-    song_array = filename.split(" - ")
-    song_array[1] = song_array[1].chomp(".mp3")
-    song = self.new
-    song.name = song_array[1]
-    song.artist_name = song_array[0]
-    song
-  end
+#   def self.new_from_filename(filename)
+#     song_array = filename.split(" - ")
+#     song_array[1] = song_array[1].chomp(".mp3")
+#     song = self.new
+#     song.name = song_array[1]
+#     song.artist_name = song_array[0]
+#     song
+#   end
 
-  def self.create_from_filename(filename)
-    result = self.new_from_filename(filename)
-    song = self.create
-    song.name = result.name
-    song.artist_name = result.artist_name
-    song
-  end
+#   def self.create_from_filename(filename)
+#     result = self.new_from_filename(filename)
+#     song = self.create
+#     song.name = result.name
+#     song.artist_name = result.artist_name
+#     song
+#   end
 
-  def self.destroy_all
-    self.all.clear
-  end
-end
+#   def self.destroy_all
+#     self.all.clear
+#   end
+# end
